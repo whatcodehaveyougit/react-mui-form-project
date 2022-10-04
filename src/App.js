9ac2c8903a74da401c909d8b4bd0994e1d2eb62f
whatcodehaveyougit <siggywatt@yahoo.com>
2022-10-04 16:08:41 +0200

    now using css variables for colors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/header/header'
 import Form from './routes/interventionForm/interventionForm.tsx'
